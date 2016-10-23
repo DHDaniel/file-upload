b141c449937f3007de4bd6150948926e7c29d4f3
Daniel Hernandez <dh.danny@hotmail.com>
2016-10-23 11:44:43 -0400

    Finished app infrastructure

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 
+/* Helpers */
+
+// This shouldn't really go here, but it's a small app, so what's the point of creating
+// another file and importing it.
+
+// from http://scratch99.com/web-development/javascript/convert-bytes-to-mb-kb/
+function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == 0) return 'n/a';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    if (i == 0) return bytes + ' ' + sizes[i];
+    return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+};
+
+
 const express = require("express");
-const bodyParser = require("body-parser");
+const multer = require("multer");
+
+// storing in memory
+// by doing this, we run the risk that the memory will max out, and our app's
+// performance may suffer, or even crash. However, since we are using Heroku
+// and have no definite way to store files besides Amazon S3 (which only allows
+// a free account up to 1 year), we will have to use this method.
+
+var storage = multer.memoryStorage();
+var upload = multer({ storage : storage});
+
 var app = express();
 
 /* Config */
 
 app.use(express.static(__dirname + "/views"));
-app.use(bodyParser());
 app.set("port", process.env.PORT || 8080);
 
 
@@ -19,9 +43,15 @@ app.get("/", function (req, res) {
 });
 
 // processing file and sending response
-app.post("/upload", function (req, res) {
-  console.log(req.files);
-  res.end("Ended");
+// name in upload.single() must match the name in the form
+app.post("/upload", upload.single('upload_file'), function (req, res) {
+  var bytesize = req.file.size;
+  var response = {
+    bytes : bytesize,
+    humanReadable : bytesToSize(bytesize)
+  };
+
+  res.end(JSON.stringify(response));
 });
 
 app.listen(app.get("port"), function () {
